57c598cbbeea4a5ae644ba9bc010510f8e2a8be6
Konstantinopola <mortimergames2007@gmail.com>
2026-04-15 00:29:23 +0300

    Update Close.java

--- Close.java
+++ Close.java
@@ -1,4 +1,20 @@
-package tu_varna.sit.f24621744.FileActions;
+package bg.tu_varna.sit.f24621744.task;
 
-public class Close {
+public class Close implements Command {
+
+    @Override
+    public void execute(String arguments, Session session) {
+        if (!session.isFileOpen()) {
+            System.out.println("There are no open files to close.");
+            return;
+        }
+
+        session.closeFile();
+        System.out.println("The file was closed successfully. Changes were discarded.");
+    }
+
+    @Override
+    public String getDescription() {
+        return "closed";
+    }
 }
